702e4b0e5afe5247495448a4ba14e74c4bd6c3ab
Hamza Amentag <hament3@wgu.edu>
2025-05-03 22:35:33 -0500

    Created and started threads displaying the welcome message in English and French

--- src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
+++ src/main/java/edu/wgu/d387_sample_code/D387SampleCodeApplication.java
@@ -8,21 +8,42 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 @SpringBootApplication
 public class D387SampleCodeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(D387SampleCodeApplication.class, args);
-		//
-		Properties properties = new Properties();
-		try{
-			InputStream  inputStream = new ClassPathResource("translation_fr.properties").getInputStream();
-			properties.load(inputStream);
-			System.out.println(properties.getProperty("welcome"));
-		}
-		catch(Exception e){e.printStackTrace();}
- 		//
+		///
+		Runnable englishWelcome = () -> {
+			try {
+				Properties properties = new Properties();
+				InputStream stream = new ClassPathResource("translation_en.properties").getInputStream();
+				properties.load(stream);
+				System.out.println(properties.getProperty("welcome"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		};
+
+		Runnable frenchWelcome = () -> {
+			try {
+				Properties properties = new Properties();
+				InputStream stream = new ClassPathResource("translation_fr.properties").getInputStream();
+				properties.load(stream);
+				System.out.println(properties.getProperty("welcome"));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		};
+
+		// Create and start threads
+		new Thread(englishWelcome).start();
+		new Thread(frenchWelcome).start();
+		////
 	}
 
 }
